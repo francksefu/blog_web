441935a1f74e8017dcab04fbae1c9166413b3392
francksefu <francksefu1998@gmail.com>
2024-01-10 23:35:32 +0200

    Correct linters

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,12 +3,12 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   def update_comments_counter
-    comment_counter = self.post.comments_counter
+    comment_counter = post.comments_counter
     if comment_counter.nil?
       comment_counter = 1
     else
       comment_counter += 1
     end
-    self.post.update(comments_counter: comment_counter)
+    post.update(comments_counter: comment_counter)
   end
 end
