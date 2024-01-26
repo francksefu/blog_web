674667a94d8f269d919f9490b4c132059709b763
francksefu <francksefu1998@gmail.com>
2024-01-26 12:04:00 +0200

    Update Ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,7 @@ class Ability
 
   def initialize(user)
     # can :all, Comment, user:
-
+    can :manage, Post, author_id: user.id
     # Define abilities for the passed in user here. For example:
 
     #   user ||= User.new # guest user (not logged in)
